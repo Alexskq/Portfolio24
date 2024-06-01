5c504c5ba4f8e0cd8c4125f6f228efd292512462
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-06-01 12:56:23 +0200

    projects section done

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
